306aedfacd94a7478713ceece41203b66f0ce51d
Kurama <renderosalexis17@gmail.com>
2018-10-20 00:50:53 -0500

    content_main is now empty, I made the data collection page a separate entity. Also threw in the recycle pngs. -Alexis

--- app/src/main/java/com/ortreport/ortreport/MainActivity.kt
+++ app/src/main/java/com/ortreport/ortreport/MainActivity.kt
@@ -2,23 +2,50 @@ package com.ortreport.ortreport
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.trackerdatacollectionpageslider_activity.*
+import kotlinx.android.synthetic.main.trackerdatacollectionpageslider_content.*
+import android.widget.Toast
+import android.widget.SeekBar
+
+
+
+
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.trackerdatacollectionpageslider_activity)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Hey! You tapped me!", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        simpleSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            var progressChangedValue = 0
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                progressChangedValue = progress
+                ortReporter.text = "$progress %"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                Toast.makeText(
+                    this@MainActivity, "Seek bar progress is :$progressChangedValue",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
