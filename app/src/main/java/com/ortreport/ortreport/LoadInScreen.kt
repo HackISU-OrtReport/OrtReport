55a484c737bd769dbd99ebda1bed0db0bd882d54
Kurama <renderosalexis17@gmail.com>
2018-10-21 00:40:58 -0500

    Pushing up what I have. I've got spinners spinning, somewhere. IDK. -Alexis

--- app/src/main/java/com/ortreport/ortreport/LoadInScreen.kt
+++ app/src/main/java/com/ortreport/ortreport/LoadInScreen.kt
@@ -3,20 +3,22 @@ package com.ortreport.ortreport
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter
+import android.widget.Button
 
 import kotlinx.android.synthetic.main.activity_load_in_screen.*
+import android.widget.Spinner
+
+
 
 class LoadInScreen : AppCompatActivity() {
+
+    private var spinner1: Spinner? = null
+    private var button: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_load_in_screen)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
-
 }
